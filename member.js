b392438438c88ee0fe87d77d5e950a9ebe904e2b
pavangandham1 <38438600+pavangandham1@users.noreply.github.com>
2024-05-28 06:13:40 +0000

    new member

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    skills: ['JavaScript', 'React', 'Node', 'MongoDB', 'Express', 'HTML', 'CSS', 'SASS'],
+    getSkills: function() {
+      return this.skills;
+    }
+  };
+}gi
